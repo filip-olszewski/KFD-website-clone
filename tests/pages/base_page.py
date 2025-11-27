74d69911b72bb3fff7959d0711101c0ebeee470a
Nikodem <s198013@student.pg.edu.pl>
2025-11-27 15:28:41 +0100

    added basic operations like click, type, find

--- tests/pages/base_page.py
+++ tests/pages/base_page.py
@@ -1,4 +1,5 @@
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
     def __init__(self, driver):
@@ -6,3 +7,18 @@ class BasePage:
 
     def open(self, url):
         self.driver.get(url)
+
+    def click(self, locator, use_js=False):
+        element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(locator))
+        if use_js:
+            self.driver.execute_script("arguments[0].click();", element)
+        else:
+            element.click()
+
+    def type(self, locator, text):
+        element = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(locator))
+        element.clear()
+        element.send_keys(text)
+
+    def find(self, locator):
+        return WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(locator))
