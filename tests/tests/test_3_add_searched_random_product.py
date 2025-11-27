f8ddd663add465e05464d4a2dc5d8d184c6cd45f
Nikodem <s198013@student.pg.edu.pl>
2025-11-27 16:24:13 +0100

    added third test to search and add product

---
+++ tests/tests/test_3_add_searched_random_product.py
@@ -0,0 +1,22 @@
+from pages.home_page import HomePage
+from pages.product_page import ProductPage
+from pages.search_page import SearchPage
+from pages.cart_page import CartPage
+
+
+def test_add_random_product(driver):
+    home = HomePage(driver)
+    product = ProductPage(driver)
+    search = SearchPage(driver)
+    cart = CartPage(driver)
+
+    home.open_home()
+    home.search("test") # to change
+    initial = cart.get_amount_of_products()
+    search.open_random_product()
+    product.add_to_cart()
+    product.continue_shopping()
+    home.open_cart()
+
+    assert initial < cart.get_amount_of_products()
+    
