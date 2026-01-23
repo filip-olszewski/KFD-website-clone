49a30bc6a80de8af36b067018876c0faaaa58b29
filip-olszewski <olszewski.fp@gmail.com>
2026-01-23 01:23:28 +0100

    Fixes GA

---
+++ html/modules/ps_googleanalytics/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/ps_googleanalytics',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '682944cfbe0604aed95629f5ad81853de260a51d',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/ps_googleanalytics' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '682944cfbe0604aed95629f5ad81853de260a51d',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
